50f2f7ccb810382ace3d992aee2ff1da6660287d
LauraMilenaAragonMesa <laura26052017>
2022-10-21 19:56:20 -0500

    Solucion Ejercicio 2

--- api.php
+++ api.php
@@ -2,12 +2,22 @@
     header("Access-Control-Allow-Origin: *");
     header('Content-Type: application/json; charset=utf-8');
 
-    // "Desarrolle un algoritmo que realice la sumatoria de los números enteros 
-    // comprendidos entre el 1 y el 1O, es decir, 1 + 2 + 3 + .... + 1O."
-    $suma = 1+2+3+4+5+6+7+8+9+10;
+    //2. Determinar la hipotenusa de un triángulo rectángulo conocidas 
+    //las longitudes de sus dos catetos. Desarrolle el algoritmo 
+    //correspondiente. Formula: (HIP = √CATa + √CATb)
+
+    $CATa = 9;
+    $CATb = 16;
+
+    $RaizCATa = sqrt($CATa);
+    $RaizCATb = sqrt($CATb);
+
+    $HIP = $RaizCATa + $RaizCATb;
+
+    
 
     $obj = (object) [
-        "Resultado" => "La sumatoria de los números enteros es : $suma ",
+        "Resultado" => "La hipotenusa de un triángulo rectángulo es : $HIP ",
         "Mensaje" => "Servidor de origen ".$_SERVER["HTTP_HOST"]
     ];
  
